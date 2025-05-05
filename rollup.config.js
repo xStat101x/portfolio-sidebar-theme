1b43e7e2aeab81b1479d29848cc134480e968e3f
Rex Kenyon <Rex@kenyon.us.com>
2025-05-04 23:53:10 -0400

    More attemps to fix vercel.

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import babel from '@rollup/plugin-babel';
 import { rollupPluginHTML as html } from '@web/rollup-plugin-html';
 import { importMetaAssets } from '@web/rollup-plugin-import-meta-assets';
 import esbuild from 'rollup-plugin-esbuild';
+import copy from 'rollup-plugin-copy';
 
 export default {
   input: 'index.html',
